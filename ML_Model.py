9a01335829bfe1604ef72a498dbc2be9ad18ee4a
Divya Kumar <143162063+DivyaKumar-26@users.noreply.github.com>
2025-10-19 17:10:33 +0530

    Document ML model strategy in ML_Model.py
    Add comments outlining the ML model approach and methodology.

--- ML_Model.py
+++ ML_Model.py
@@ -1,1 +1,12 @@
+# https://data.mendeley.com/datasets/8v757rr4st/1/files/98242fd3-1912-4a59-ab26-23d97b454218
+
+# Use dataset from above website randomly split it into two paritions, try to use 10 cross-validations( or any other better method you know to maximize the output!!)
+
+# We will be using the training dataset to train this ML model and use regression on same training dataset to get weights for different soil features, then we will use derrived wieghts to model our soil bio sensor.
+# We need to use complex regression methods to capture more complexities of data along with that use regularization to avoid overfitting 
+
+# Then we will have final showdown between these two models, on testing dataset. 
+
+# Final Score will be given taking ML model as the base.
+
 
