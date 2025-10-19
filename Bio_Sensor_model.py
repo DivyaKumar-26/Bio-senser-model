8e51bcf83908344da1ea9c4c74a54d790dd8b596
Divya Kumar <143162063+DivyaKumar-26@users.noreply.github.com>
2025-10-19 17:14:51 +0530

    Create Bio_Sensor_model.py

---
+++ Bio_Sensor_model.py
@@ -0,0 +1,4 @@
+# We will be using wieghts derrived from Regression to make model for our bio sensor, modelling will be done using brian2.
+# We will then use the model on training dataset to get the desired output and scores will be compared will ML model.
+
+# Basically We have to show that our model works better than ML model to show that our biosensor is something better.
