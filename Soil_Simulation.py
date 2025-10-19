bdd69a703ed965580e2ef2a627249693be283285
Divya Kumar <143162063+DivyaKumar-26@users.noreply.github.com>
2025-10-19 16:52:58 +0530

    Update Soil_Simulation.py

--- Soil_Simulation.py
+++ Soil_Simulation.py
@@ -1,1 +1,10 @@
+# https://data.mendeley.com/datasets/8v757rr4st/1/files/98242fd3-1912-4a59-ab26-23d97b454218 
+# Use above website for soil recommendations.
 
+
+# Use any one of these methods or other methods for soil simulation.
+
+# https://github.com/SIADSiM/PythonToolsForSoilScienceModeling
+# https://medium.com/@jgcoded/analyzing-soil-data-with-python-pandas-and-pygwalker-cfac76f20e74
+# https://towardsdatascience.com/build-soil-profile-in-plaxis-using-python-a9c870e253c7/
+# https://www.kaggle.com/code/patimejia/soil-data-a-basic-python-pipeline
